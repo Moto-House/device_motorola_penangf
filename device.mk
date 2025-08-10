35096e8e413584c0ecc7ad35f3fc05dfeb051bf6
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-08-10 09:11:25 -0400

    penangf: Import rootdir from stock
    Change-Id: Iee9d1f1275978cd028e6fd18e8cf139681c38490
    Signed-off-by: GitFASTBOOT <ahmedshahinx0x@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,35 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    factory_init.connectivity.common.rc \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.SNxxx.nfc.rc \
+    init.SNxxx.se.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6768.rc \
+    init.mt6768.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    init_connectivity.rc \
+    meta_init.connectivity.common.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    meta_init.vendor.rc \
+    multi_init.rc \
+    fstab.mt6768
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
