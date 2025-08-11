055e567bd11dedc5532db79b0786d1bdc05d0a91
Mashopy <eliasgheeraert@gmail.com>
2025-08-10 22:57:44 -0400

    penangf: Inherit mediatek common sepolicy
    Change-Id: Ibfc17d13d8bc97f3db102d9a5d8d12bdb14bc1e3
    Signed-off-by: GitFASTBOOT <ahmedshahinx0x@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2025-06-05
 
+# Sepolicy
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
