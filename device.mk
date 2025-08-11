dcdcaea7b3cc5d1ee05578fe8fe1be86bb47fe40
luk1337 <priv.luk@gmail.com>
2025-08-10 22:52:34 -0400

    penangf: Build vendor libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: Id646a2d2bb5aa58fb6f0dc79a33781e43423c298
    Signed-off-by: GitFASTBOOT <ahmedshahinx0x@gmail.com>

--- device.mk
+++ device.mk
@@ -145,6 +145,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
 # Keystore
 PRODUCT_PACKAGES += \
     libcppbor_external.vendor \
