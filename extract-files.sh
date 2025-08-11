fb89b02efc9d72ac7736266611faecd239a9358c
bengris32 <bengris32@protonmail.ch>
2025-08-10 23:26:57 -0400

    penangf: Use enable instead of start for Neural Networks service
    * The MTK Neural Networks service is started via a property trigger.
      This HAL starts a lot earlier than other HALs since it is started
      with start instead of enable, the difference between them being
      that start will immediately cause the service to try and start, while
      enable effectively removes the disabled attribute from the service
      (so in the case of a HAL, it will be started with all the other services
      in class hal instead of immediately).
    
    * This initially didn't have any side effects until I noticed the Neural Networks
      service constantly crashing in offline charger mode. To fix this, simply
      convert the start command to an enable to ensure that this service is not
      only started at the right time (in class hal), but also only started when
      needed (and not in offline charger mode).
    
    Change-Id: I2f0e993ba8c0004090c2a924e2be54642184069d
    Signed-off-by: GitFASTBOOT <ahmedshahinx0x@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup {
+    case "$1" in
+        vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
+            sed -i 's/start/enable/' "$2"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
