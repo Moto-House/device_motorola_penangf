6ed6d07aa6d986c30247443f45baa727a423f0d1
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-08-13 18:16:31 -0400

    penangf: Build BesLoudness
    Change-Id: I779faa04d18d991b5fda162d812cb71ac70759b5

--- device.mk
+++ device.mk
@@ -82,7 +82,8 @@ PRODUCT_PACKAGES += \
     libtinycompress
 
 PRODUCT_PACKAGES += \
-    BesLoudness
+    BesLoudness \
+    DolbyManager
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
