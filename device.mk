e2877931ae2c5c8772fc7002e0e74a25953915b7
bengris32 <bengris32@protonmail.ch>
2025-08-16 17:45:00 -0400

    penangf: Build OSS MediaTek USB service
    Change-Id: Ib9807dffafb5b1bb33ed27a245569603dbb5c0e4
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -382,8 +382,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb.gadget@1.0.vendor \
-    android.hardware.usb@1.3.vendor
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
 TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
