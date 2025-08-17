8ae255383eec559da436ab939f41ef798b27a836
SamarV-121 <samar@samarv121.dev>
2025-08-16 23:04:11 -0400

    penangf: Rename libwifi-hal.so to prevent conflict with AOSP
    Change-Id: I9f4cac61d43d1ed92620e211f9107d8de83bb105

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,9 @@ function blob_fixup {
         vendor/lib64/libcam.utils.sensorprovider.so)
             grep -q "libshim_sensors.so" "$2" || "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
+        vendor/lib64/libwifi-hal-mtk.so)
+            "${PATCHELF}" --set-soname "libwifi-hal-mtk.so" "${2}"
+            ;;
         vendor/lib64/libmtkcam_featurepolicy.so)
             # evaluateCaptureConfiguration()
             printf '\x28\x02\x80\x52' | dd of="$2" bs=1 seek=$((0x3e828)) count=4 conv=notrunc
