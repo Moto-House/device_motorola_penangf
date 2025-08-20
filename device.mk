5ded276510591b697f25f02838fc7a301b1679d6
bengris32 <bengris32@protonmail.ch>
2025-08-19 20:14:52 -0400

    penangf: Import AOSP fingerprint service
    Change-Id: Ifcf65b268eeb1cca70ee9eb2a27056705398f968

--- device.mk
+++ device.mk
@@ -158,7 +158,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1-service.penangf
 
 PRODUCT_PACKAGES += \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
