742eca16ba4bcc2b019f16b6c76651579e6b0153
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-09-01 13:52:10 -0400

    penangf: Reconfigure VINTF stack
    * Include mediatek_framework_compatibility_matrix
    
    Change-Id: Ia1dd805ae6f0951f1fa97b320e146d57a6aaab6c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,10 @@ TARGET_NO_BOOTLOADER := true
 TARGET_SCREEN_DENSITY := 280
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
