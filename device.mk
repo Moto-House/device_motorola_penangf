44d3a3f9331923eb8d3233a3a7ff5e4290256d8f
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-09-03 13:30:42 -0400

    penangf: * Copy priv-app permissions for MediaTek IMS
    Change-Id: I40ad19fdbd96284bbea6fbd56a566c0215a49089

--- device.mk
+++ device.mk
@@ -220,6 +220,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor \
