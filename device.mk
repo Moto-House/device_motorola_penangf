38e8f6da54714e7f89c5280a621313435fd758e1
HollowAOSP <ahmedshahinx0x@gmail.com>
2025-10-12 02:36:23 -0400

    penangf: Initial overlays

--- device.mk
+++ device.mk
@@ -294,6 +294,19 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
     android.hardware.secure_element@1.2.vendor
 
+# Overlays
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayPenangf \
+    FrameworksResOverlayPenangf \
+    SystemUIOverlayPenangf \
+    TelephonyOverlayPenangf \
+    WifiResOverlayPenangf
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
